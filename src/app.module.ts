a1f12b07fd329150b94d6943d5fe7bd4008d600a
RadekM <radekmiedzinski@gmail.com>
2025-03-29 19:36:23 +0100

    02.04 MySQL database connection established

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,29 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'mysql',
+        host: configService.get<string>('DB_HOST'),
+        port: configService.get<number>('DB_PORT'),
+        username: configService.get<string>('DB_USER'),
+        password: configService.get<string>('DB_PASSWORD'),
+        database: configService.get<string>('DB_NAME'),
+        // entities: [__dirname + '/**/*.entity{.ts,.js}'],
+        synchronize: configService.get<string>('ENVIROMENT') === 'dev',
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
